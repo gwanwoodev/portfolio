e1e721da089ed5670464a1083c009ba0911a8a6b
gwanwoodev <gwanwoodev@kakao.com>
2020-03-17 15:43:12 +0900

    add TopAppBar & MDC Icons & Download MDC Fonts

---
+++ pages/_app.ts
@@ -0,0 +1,7 @@
+import '@material/react-top-app-bar/dist/top-app-bar.css';
+import '@material/react-material-icon/dist/material-icon.css';
+import "../public/css/material_icons.css";
+
+import HelloComponent from "./hello";
+
+export default HelloComponent;
--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,10 +1,33 @@
-import React, {Component} from 'react';
+import TopAppBar, { TopAppBarFixedAdjust, TopAppBarIcon, TopAppBarRow, TopAppBarSection } from '@material/react-top-app-bar';
+import MaterialIcon from "@material/react-material-icon";
 
-class HelloComponent extends Component {
-    render() {
-        return(
-            <div>Hello!</div>
-        )
-    }
-}
-export default HelloComponent;
+const MyComponent = () => {
+    return (
+      <div>
+        <TopAppBar>
+          <TopAppBarRow>
+            <TopAppBarSection align='start'>
+              <TopAppBarIcon navIcon tabIndex={0}>
+                <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')}/>
+              </TopAppBarIcon>
+            </TopAppBarSection>
+            <TopAppBarSection align='end' role='toolbar'>
+              <TopAppBarIcon actionItem tabIndex={0}>
+                <MaterialIcon 
+                  aria-label="print page" 
+                  hasRipple 
+                  icon='print' 
+                  onClick={() => console.log('print')}
+                />
+              </TopAppBarIcon>
+            </TopAppBarSection>
+          </TopAppBarRow>
+        </TopAppBar>
+        <TopAppBarFixedAdjust>
+          My exciting content!
+        </TopAppBarFixedAdjust>
+      </div>
+    );
+  }
+
+export default MyComponent;
