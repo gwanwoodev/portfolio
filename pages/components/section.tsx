265f200f4a8f13aaf538cae198fc1a937ad27213
gwanwoodev <gwanwoodev@kakao.com>
2020-03-20 13:11:32 +0900

    update header-section -> add header-text

--- pages/components/section.tsx
+++ pages/components/section.tsx
@@ -5,6 +5,11 @@ const MyAppSection = () => {
         <div className="header-section">
             <span className="header-title">Gwanwoodev </span>
             <span className={cn('header-title', 'bold')}>Portfolio</span>
+            <p className={cn('header-title', 'header-text')}>
+                Lorem ipsum dolor sit amet. consectetur adlpiscing eLit.
+                Suspendlsse dignissim consectetur feugiat.
+                Fusce quis ex fringitta. faciUsis velit et.
+            </p>
         </div>
     );
 }
