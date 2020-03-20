f815535a9ad3361b0866fa1abaf81cccf977e6b5
gwanwoodev <gwanwoodev@kakao.com>
2020-03-20 15:46:52 +0900

    add grid,cell,row & update cards layout

--- pages/components/card.tsx
+++ pages/components/card.tsx
@@ -1,7 +1,31 @@
-const MyAppCard = (_props:any) => {
-    return(
-        <div>Card</div>
-    )
+import React from 'react';
+import Card, {
+    CardPrimaryContent,
+    CardMedia,
+    CardActions,
+    CardActionButtons,
+    CardActionIcons
+  } from "@material/react-card";
+
+const MyAppCard = (_props: any) => {
+    return (
+        <Card>
+            <CardPrimaryContent>
+                <h1>Header</h1>
+                <CardMedia square imageUrl="https://gwanwoodev.github.io/assets/img/posts/momentum_base_placehold.jpg" />
+            </CardPrimaryContent>
+
+            <CardActions>
+                <CardActionButtons>
+                    <button>Click Me</button>
+                </CardActionButtons>
+            </CardActions>
+
+            <CardActionIcons>
+                <i>Click Me Too!</i>
+            </CardActionIcons>
+        </Card>
+    );
 }
 
 export default MyAppCard;
--- pages/components/cards.tsx
+++ pages/components/cards.tsx
@@ -1,21 +1,23 @@
 import Card from "./card";
-
-const dummySrc = "https://gwanwoodev.github.io/assets/img/posts/momentum_base_thumb.jpg";
-const previewText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+import {Cell, Grid, Row} from '@material/react-layout-grid';
 
 const MyAppCards = (_props:any) => {
     return(
         <div className="contents-cards">
-            <Card title="lorem ipsum 1" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 2" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 3" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 4" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 5" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 6" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 7" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 8" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 9" imgSrc={dummySrc} previewText={previewText} />
-            <Card title="lorem ipsum 10" imgSrc={dummySrc} previewText={previewText} />
+            <Grid>
+                <Row>
+                    <Cell columns={4}>
+                        <Card />
+                    </Cell>
+                    <Cell columns={4}>
+                        <Card />
+                    </Cell>
+
+                    <Cell columns={4}>
+                        <Card />
+                    </Cell>                                                        
+                </Row>
+            </Grid>
         </div>
     )
 }
