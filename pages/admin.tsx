5a6b0166ebf60a128a140d74b4e9a18c10e65178
gwanwoodev <gwanwoodev@kakao.com>
2020-04-10 12:04:46 +0900

    admin page porting functional components

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
+const Admin = () => (
+    <div>
+        <h1>컨텐츠 등록</h1>
+        <form action="/api/content" method="POST" encType="multipart/form-data">
+            <input type="text" name="title" />
+            <input type="text" name="preview" />
+            <input type="text" name="directLink" />
+            <input type="file" name="thumbnail" />
+            <input type="submit" />
+        </form>
+    </div>    
+)
 
-class PortfolioAdmin extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        return(
-            <div>
-                <h1>컨텐츠 등록</h1>
-                <form action="/api/content" method="POST" encType="multipart/form-data">
-                    <input type="text" name="title" />
-                    <input type="text" name="preview" />
-                    <input type="text" name="directLink" />
-                    <input type="file" name="thumbnail" />
-                    <input type="submit" />
-                </form>
-            </div>
-        )
-    }
-}
-
-export default PortfolioAdmin;
+export default Admin;
